2718e8813df42ae384d4ca3a1a045636fac78d83
Adam Thieme <adam@cs.ucla.edu>
2025-06-11 23:42:41 -0700

    styling fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <title>Campfire</title>
+        {/*<link rel={manifest} href="manifest.json"></link>*/}
+    </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
